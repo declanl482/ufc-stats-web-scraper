3cdee08d3c2d82fb30a32ef7a6538091f261c1ab
declanl482 <declanl482@gmail.com>
2024-07-10 17:38:54 -0400

    implemented event handler for Colly upon successful completion of scraping

--- scrapers/completed_ufc_event_scraper.go
+++ scrapers/completed_ufc_event_scraper.go
@@ -1,6 +1,9 @@
 package scrapers
 
 import (
+	"encoding/csv"
+	"log"
+	"os"
 	"ufc-stats-web-scraper/models"
 
 	"github.com/gocolly/colly"
@@ -47,6 +50,47 @@ func ScrapeCompletedEvents() {
 	// Create an event handler for when a page has been scraped. We will be writing the scraped data to .csv files.
 	c.OnScraped(func(r *colly.Response) {
 
+		// Create a .csv file for the scraped UFC events in the data directory.
+		file, err := os.Create("data/completed_ufc_events.csv")
+
+		// Handle any errors in generating the .csv file.
+		if err != nil {
+			log.Fatalln("Failed to create completed_ufc_events.csv file: ", err)
+		}
+
+		// Ensure that the .csv file is closed when the surrounding function returns or errors out.
+		defer file.Close()
+
+		// Create a CSV writer object to write data to the .csv file.
+		writer := csv.NewWriter(file)
+
+		// Generate and write the column headers for the output file.
+		headers := []string{
+			"event_url",
+			"event_title",
+			"event_date",
+			"event_location",
+		}
+		writer.Write(headers)
+
+		// Write the scraped UFC event data to the output file.
+		// The range keyword in Go allows us to iterate over structures like slices. It returns the index, element for each element in the slice.
+		// We can ignore the index here since it is not relevant to the data we are looking to extract.
+		for _, ufcEvent := range ufcEvents {
+			record := []string{
+				ufcEvent.EventURL,
+				ufcEvent.EventTitle,
+				ufcEvent.EventDate,
+				ufcEvent.EventLocation,
+			}
+			writer.Write(record)
+		}
+
+		// Ensure that all data from the writer object is flushed to the output file when the surrounding function returns or errors out.
+		defer writer.Flush()
 	})
+
+	// The Visit function starts the life cycle of Colly.
+	c.Visit("http://www.ufcstats.com/statistics/events/completed?page=all")
 
 }
