bbf41732369ceb72b41ec916b2d4fb3ea4d8c30c
declanl482 <declanl482@gmail.com>
2024-07-10 17:12:37 -0400

    implemented logic for parsing HTML for completed events

--- scrapers/completed_ufc_event_scraper.go
+++ scrapers/completed_ufc_event_scraper.go
@@ -18,6 +18,25 @@ func ScrapeCompletedEvents() {
 	// Create an event handler for scraping UFC event data from table rows.
 	c.OnHTML("tr.b-statistics__table-row", func(e *colly.HTMLElement) {
 
+		// Create an empty UFC event structure.
+		ufcEvent := models.UFCEvent{}
+
+		// Scrape fields from the HTML page. UFC event data is stored in table rows on the website.
+
+		// Parse the href attribute from the <a> element in the table row.
+		ufcEvent.EventURL = e.ChildAttr("a", "href")
+
+		// Parse the text context from the <a> element in the table row.
+		ufcEvent.EventTitle = e.ChildText("a")
+
+		// Parse the text from the "b-statistics__date" class in the table row.
+		ufcEvent.EventDate = e.ChildText(".b-statistics__date")
+
+		// Parse the text from the 2nd table data element in the table row.
+		ufcEvent.EventLocation = e.ChildText("td:nth-child(2)")
+
+		// Append the current UFC event to the slice of UFC events.
+		ufcEvents = append(ufcEvents, ufcEvent)
 	})
 
 	// Create an event handler for when a page has been scraped. We will be writing the scraped data to .csv files.
